a1ca7ce4b3c0018608869adfa7dc7130e3b7384c
Vadim Filimonov <philimonovvadim@gmail.com>
2020-11-26 22:08:50 +0300

    add abstract for sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 import logo from './logo.png';
+import Sidebar from './components/Sidebar';
 import Tickets from './components/Tickets';
 
 const StyledApp = styled.div`
@@ -33,74 +34,6 @@ const Main = styled.main`
 
 const Aside = styled.aside`
   grid-area: aside;
-`;
-
-const Sidebar = styled.section`
-  padding: 20px 0;
-  background: #fff;
-  border-radius: 5px;
-  box-shadow: 0px 2px 8px rgba(0, 0, 0, .1);
-`;
-
-const SidebarTitle = styled.h2`
-  box-sizing: border-box;
-  margin: 0 0 20px;
-  padding: 0 20px;
-  color: #4a4a4a;
-  font-weight: 600;
-  font-size: 12px;
-  line-height: 1;
-  letter-spacing: .5px;
-  text-transform: uppercase;
-`;
-
-const SidebarCheckbox = styled.input`
-  position: absolute;
-  width: 1px;
-  height: 1px;
-  margin: -1px;
-  clip: rect(0 0 0 0);
-`;
-
-const SidebarLabel = styled.label`
-  position: relative;
-  display: block;
-  box-sizing: border-box;
-  padding: 0 20px 0 50px;
-  color: #4a4a4a;
-  font-size: 13px;
-  line-height: 40px;
-  cursor: pointer;
-  user-select: none;
-
-  &:before {
-    position: absolute;
-    top: 50%;
-    left: 20px;
-    box-sizing: border-box;
-    width: 20px;
-    height: 20px;
-    margin-top: -10px;
-    border: 1px solid #9abbce;
-    border-radius: 2px;
-    content: "";
-  }
-
-  &:hover {
-    background: #f1fcff;
-  }
-
-  ${SidebarCheckbox}:focus + & {
-    outline: -webkit-focus-ring-color auto 1px;
-  }
-
-  ${SidebarCheckbox}:checked + &:before {
-    background-image: url("/images/checked.svg");
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: 12px auto;
-    border-color: #2196f3;
-  }
 `;
 
 const ButtonGroup = styled.div`
@@ -157,44 +90,7 @@ const App = () => {
         <Tickets searchId={searchId} />
       </Main>
       <Aside>
-        <Sidebar>
-          <SidebarTitle>Количество пересадок</SidebarTitle>
-          <SidebarCheckbox
-            id="all"
-            value="all"
-            type="checkbox"
-            name="transfers"
-          />
-          <SidebarLabel htmlFor="all">Все</SidebarLabel>
-          <SidebarCheckbox
-            id="transfer0"
-            value="transfer0"
-            type="checkbox"
-            name="transfers"
-          />
-          <SidebarLabel htmlFor="transfer0">Без пересадок</SidebarLabel>
-          <SidebarCheckbox
-            id="transfer1"
-            value="transfer1"
-            type="checkbox"
-            name="transfers"
-          />
-          <SidebarLabel htmlFor="transfer1">1 пересадка</SidebarLabel>
-          <SidebarCheckbox
-            id="transfer2"
-            value="transfer2"
-            type="checkbox"
-            name="transfers"
-          />
-          <SidebarLabel htmlFor="transfer2">2 пересадки</SidebarLabel>
-          <SidebarCheckbox
-            id="transfer3"
-            value="transfer3"
-            type="checkbox"
-            name="transfers"
-          />
-          <SidebarLabel htmlFor="transfer3">3 пересадки</SidebarLabel>
-        </Sidebar>
+        <Sidebar />
       </Aside>
     </StyledApp>
   );
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+  padding: 20px 0;
+  background: #fff;
+  border-radius: 5px;
+  box-shadow: 0px 2px 8px rgba(0, 0, 0, .1);
+`;
+
+const Title = styled.h2`
+  box-sizing: border-box;
+  margin: 0 0 20px;
+  padding: 0 20px;
+  color: #4a4a4a;
+  font-weight: 600;
+  font-size: 12px;
+  line-height: 1;
+  letter-spacing: .5px;
+  text-transform: uppercase;
+`;
+
+const Checkbox = styled.input`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  clip: rect(0 0 0 0);
+`;
+
+const Label = styled.label`
+  position: relative;
+  display: block;
+  box-sizing: border-box;
+  padding: 0 20px 0 50px;
+  color: #4a4a4a;
+  font-size: 13px;
+  line-height: 40px;
+  cursor: pointer;
+  user-select: none;
+
+  &:before {
+    position: absolute;
+    top: 50%;
+    left: 20px;
+    box-sizing: border-box;
+    width: 20px;
+    height: 20px;
+    margin-top: -10px;
+    border: 1px solid #9abbce;
+    border-radius: 2px;
+    content: "";
+  }
+
+  &:hover {
+    background: #f1fcff;
+  }
+
+  ${Checkbox}:focus + & {
+    outline: -webkit-focus-ring-color auto 1px;
+  }
+
+  ${Checkbox}:checked + &:before {
+    background-image: url("/images/checked.svg");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 12px auto;
+    border-color: #2196f3;
+  }
+`;
+
+const Sidebar = () => (
+  <Wrapper>
+    <Title>Количество пересадок</Title>
+    <Checkbox
+      id="all"
+      value="all"
+      type="checkbox"
+      name="transfers"
+    />
+    <Label htmlFor="all">Все</Label>
+    <Checkbox
+      id="transfer0"
+      value="transfer0"
+      type="checkbox"
+      name="transfers"
+    />
+    <Label htmlFor="transfer0">Без пересадок</Label>
+    <Checkbox
+      id="transfer1"
+      value="transfer1"
+      type="checkbox"
+      name="transfers"
+    />
+    <Label htmlFor="transfer1">1 пересадка</Label>
+    <Checkbox
+      id="transfer2"
+      value="transfer2"
+      type="checkbox"
+      name="transfers"
+    />
+    <Label htmlFor="transfer2">2 пересадки</Label>
+    <Checkbox
+      id="transfer3"
+      value="transfer3"
+      type="checkbox"
+      name="transfers"
+    />
+    <Label htmlFor="transfer3">3 пересадки</Label>
+  </Wrapper>
+);
+
+export default Sidebar;
