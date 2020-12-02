f4c509d5fe0003208b0fae362171598c8da6598b
Vadim Filimonov <philimonovvadim@gmail.com>
2020-12-02 09:28:27 +0300

    add draft fetching chunks

--- src/components/Tickets/index.js
+++ src/components/Tickets/index.js
@@ -5,6 +5,8 @@ import styled from 'styled-components';
 import axios from 'axios';
 import Ticket from '../Ticket';
 
+const COUNT_TICKETS_TO_SHOW = 5;
+
 const Wrapper = styled.section`
   display: grid;
   row-gap: 20px;
@@ -13,14 +15,31 @@ const Wrapper = styled.section`
 const Tickets = (props) => {
   const { searchId, params } = props;
   const [tickets, setTickets] = useState([]);
+  const [finished, setFinished] = useState(false);
 
   useEffect(() => {
     const fetchTickets = async () => {
-      const response = await axios.get(`https://front-test.beta.aviasales.ru/tickets?searchId=${searchId}`);
-      setTickets(response.data.tickets);
+      const response = await axios.get(`https://front-test.beta.aviasales.ru/tickets?searchId=${searchId}`)
+        .catch((e) => {
+          if (e.response) {
+            // eslint-disable-next-line no-console
+            console.error(e.response.status);
+          }
+        });
+      const newTickets = response ? response.data.tickets : [];
+      setTickets((t) => [...t, ...newTickets]);
+      if (response?.data.stop) {
+        setFinished(true);
+      } else {
+        fetchTickets();
+      }
     };
     fetchTickets();
   }, []);
+
+  if (!finished) {
+    return null;
+  }
 
   return (
     <Wrapper>
@@ -33,9 +52,9 @@ const Tickets = (props) => {
           (stopCount) => ticketStopsCount.includes(stopCount),
         );
         return intersection > 0;
-      }).map((ticket) => (
-        <Ticket ticket={ticket} key={uuid()} />
-      ))}
+      })
+        .slice(0, COUNT_TICKETS_TO_SHOW)
+        .map((ticket) => <Ticket ticket={ticket} key={uuid()} />)}
     </Wrapper>
   );
 };
